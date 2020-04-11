1d60a9faccbd64c65190b1f150e948532b1c661b
Melanie Hallowell <melleehall@gmail.com>
2020-04-11 12:25:24 -0600

    Mid-refactoring of POST request and debugging issue with connection when running tests

--- src/app.js
+++ src/app.js
@@ -4,16 +4,59 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const ArticlesService = require('./articles-service')
 
 const app = express()
+const jsonParser = express.json()
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
 app.use(morgan(morganOption))
-app.use(helmet())
 app.use(cors())
+app.use(helmet())
+
+app.get('/articles', (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    ArticlesService.getAllArticles(knexInstance)
+        .then(articles => {
+            res.json(articles)
+        })
+        // note that we pass next into the .catch from the promise 
+        // chain so any errors are handled by our error handler middleware
+        .catch(next)
+})
+
+app.post('/articles', jsonParser, (req, res, next) => {
+    const { title, content, style } = req.body
+    const newArticle = { title, content, style }
+    ArticlesService.insertArticle(
+        req.app.get('db'),
+        newArticle
+    )
+        .then(article => {
+            res
+                .status(201)
+                .location(`/articles/${article.id}`)
+                .json(article)
+        })
+        .catch(next)
+})
+
+app.get('/articles/:article_id', (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    ArticlesService.getById(knexInstance, req.params.article_id)
+        .then(article => {
+            if(!article) {
+                return res.status(404).json({
+                    error: { message: `Article doesn't exist` }
+                })
+            }
+            res.json(article)
+        })
+        .catch(next)
+})
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
--- src/server.js
+++ src/server.js
@@ -1,5 +1,17 @@
+// this is the controller file
+
+// We are creating the Knex instance here so the connection 
+// is explicit rather than implicit
+const knex = require('knex')
 const app = require('./app')
-const { PORT } = require('./config')
+const { PORT, DB_URL } = require('./config')
+
+const db = knex({
+  client: 'pg',
+  connection: DB_URL,
+})
+
+app.set('db', db)
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
