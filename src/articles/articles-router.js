f780019708825b134e808b203d9eba1cac3f8e7f
Melanie Hallowell <melleehall@gmail.com>
2020-04-27 21:57:44 -0600

    Accidentally initialized empty git repo so adding current code

---
+++ src/articles/articles-router.js
@@ -0,0 +1,120 @@
+const path = require('path')
+const express = require('express')
+const xss = require('xss')
+const ArticlesService = require('./articles-service')
+
+const articlesRouter = express.Router()
+const jsonParser = express.json()
+
+const serializeArticle = article => ({
+  id: article.id,
+  style: article.style,
+  title: xss(article.title),
+  content: xss(article.content),
+  date_published: article.date_published,
+})
+
+articlesRouter
+  .route('/')
+  .get((req, res, next) => {
+    ArticlesService.getAllArticles(
+      req.app.get('db')
+    )
+      .then(articles => {
+        res.json(articles.map(serializeArticle))
+      })
+      .catch(next)
+  })
+  .post(jsonParser, (req, res, next) => {
+    const { title, content, style } = req.body
+    const newArticle = { title, content, style }
+
+    for (const [key, value] of Object.entries(newArticle)) {
+      if (value == null) {
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` }
+        })
+      }
+    }
+
+    sanitizedArticle = {
+      title: xss(title),
+      content: xss(content),
+      style: style,
+    }
+
+    ArticlesService.insertArticle(
+      req.app.get('db'),
+      sanitizedArticle
+    )
+      .then(article => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${article.id}`))
+          .json(article)
+      })
+      .catch(next)
+  })
+
+articlesRouter
+  .route('/:article_id')
+  .all((req, res, next) => {
+    ArticlesService.getById(
+      req.app.get('db'),
+      req.params.article_id
+    )
+    .then(article => {
+      if (!article) {
+        return res.status(404).json({
+          error: { message: `Article does not exist` }
+        })
+      }
+      res.article = article //save the article for the next middleware
+      next() // don't forget to call next so the next middleware happens!
+    })
+    .catch(next)
+  })
+  .get((req, res, next) => {
+    res.json({
+      id: res.article.id,
+      style: res.article.style,
+      title: xss(res.article.title),
+      content: xss(res.article.content),
+      date_published: res.article.date_published,
+    })
+  })
+  .delete((req, res, next) => {
+    ArticlesService.deleteArticle(
+      req.app.get('db'),
+      req.params.article_id
+    )
+      .then(() => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
+  .patch(jsonParser, (req, res, next) => {
+    const { title, content, style } = req.body
+    const articleToUpdate = { title, content, style }
+    
+    const numberOfValues = Object.values(articleToUpdate).filter(Boolean).length
+    if (numberOfValues === 0) {
+      return res.status(400).json({
+        error: {
+          message: `Request body must contain either 'title', 'style', or 'content'`
+        }
+      })
+    }
+
+    ArticlesService.updateArticle(
+      req.app.get('db'),
+      req.params.article_id,
+      articleToUpdate
+    )
+    .then(numRowsAffected => {
+      res.status(204).end()
+    })
+    .catch(next)
+  })
+
+module.exports = articlesRouter
